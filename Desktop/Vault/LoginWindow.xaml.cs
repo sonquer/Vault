67082312e33a52a734ade17604c4b7501a31f776
kamilagruhlke <kamilagruhlke@gmail.com>
2019-12-17 21:55:22 +0100

    Added material design prototype

--- Desktop/Vault/LoginWindow.xaml.cs
+++ Desktop/Vault/LoginWindow.xaml.cs
@@ -10,24 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Vault
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for LoginWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LoginWindow : Window
     {
-        public MainWindow()
+        public LoginWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            transitioner.SelectedIndex = 1;
         }
     }
 }
