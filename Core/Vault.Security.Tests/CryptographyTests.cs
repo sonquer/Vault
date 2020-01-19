90fc522e5b680127dd536b2a16e3f7eb2e698186
kamilagruhlke <kamilagruhlke@gmail.com>
2020-01-19 17:25:47 +0100

    Added cryptography tests

---
+++ Core/Vault.Security.Tests/CryptographyTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Vault.Security.Tests
+{
+    public class CryptographyTests
+    {
+        [TestCase]
+        public void Encrypt_and_decrypt_test()
+        {
+            var secret = "secret text";
+
+            var encryptedData = Cryptography.Encrypt(Encoding.UTF8.GetBytes(secret), "password");
+
+            var decryptedData = Cryptography.Decrypt(encryptedData, "password");
+
+            Assert.AreEqual(secret, Encoding.UTF8.GetString(decryptedData));
+        }
+
+        [TestCase]
+        public void Wrong_password_test()
+        {
+            var secret = "secret text";
+
+            var encryptedData = Cryptography.Encrypt(Encoding.UTF8.GetBytes(secret), "password");
+
+            Assert.Catch<CryptographicException>(() =>
+            {
+                Cryptography.Decrypt(encryptedData, "123456");
+            });
+        }
+    }
+}
