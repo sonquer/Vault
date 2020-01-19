aa75fc615c5af3a923f924f99ef12e7ec645dac9
kamilagruhlke <kamilagruhlke@gmail.com>
2020-01-19 17:16:03 +0100

    Added recovery code tests

---
+++ Core/Vault.Security.Tests/Random/RecoveryCodeGeneratorTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using Vault.Security.Random;
+
+namespace Vault.Security.Tests.Random
+{
+    public class RecoveryCodeGeneratorTests
+    {
+        [TestCase]
+        public void Random_hash_tests()
+        {
+            var random1 = RecoveryCodeGenerator.Instance.Generate();
+            var random2 = RecoveryCodeGenerator.Instance.Generate();
+
+            Assert.IsFalse(random1 == random2);
+        }
+    }
+}
